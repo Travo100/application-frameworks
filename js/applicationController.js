8a73ffe97f684af93a0c9a18f937a7252da4a3f7
unknown <tthompson@RA7938L.renovateamerica.com>
2015-04-27 14:54:46 -0700

    removed commented code from index.html

--- js/applicationController.js
+++ js/applicationController.js
@@ -1,9 +1,9 @@
 app.controller('applicationController', function(){
 	var vm = this; 
 	vm.submitForm = function(data){
-	  if(vm.applicationForm.$valid){
-	    vm.masterFile = angular.copy(data);
-	    alert('Form submit!');
-	 }
+		if(vm.applicationForm.$valid){
+			vm.masterFile = angular.copy(data);
+			alert('Form submit!');
+		}
 	};
 });
